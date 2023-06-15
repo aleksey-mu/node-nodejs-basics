777568cbc22d69b3306e4bdc7d60a5c1933d86d2
Aleksei Patsuk <aleksei.patsuk@ukg.com>
2023-06-16 00:44:09 +0300

    add 'rename'

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,35 @@
+import * as fs from "node:fs";
+
 const rename = async () => {
-    // Write your code here 
+  const wrongFileNamePath = "./files/wrongFilename.txt";
+  const newFileNamePath = "./files/properFilename.md";
+  const errorMsg = "FS operation failed";
+
+  fs.access(wrongFileNamePath, (err) => {
+    if (err) {
+      const error = new Error(errorMsg);
+
+      console.error(error);
+      return;
+    }
+
+    fs.access(newFileNamePath, (err) => {
+      if (!err) {
+        const error = new Error(errorMsg);
+
+        console.error(error);
+        return;
+      }
+
+      fs.rename(wrongFileNamePath, newFileNamePath, (err) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+        console.log("Rename complete!");
+      });
+    });
+  });
 };
 
 await rename();
