e909b55bb09fa768d0a61b0996df975831aa4244
Aleksei Patsuk <aleksei.patsuk@ukg.com>
2023-06-15 23:53:37 +0300

    add 'create'

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,21 @@
+import * as fs from "node:fs";
+
 const create = async () => {
-    // Write your code here 
+  const fileName = "files/fresh.txt";
+  const content = "I am fresh and young";
+
+  fs.readFile(fileName, (err, data) => {
+    if (data) {
+      const error = new Error("FS operation failed");
+
+      console.error(error);
+      return;
+    }
+
+    fs.writeFile(fileName, content, (err) => {
+      err ? console.error(err) : console.log("File created!");
+    });
+  });
 };
 
 await create();
