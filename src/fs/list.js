0013fddc3916f6f46968019ee3af66fb7cd5c7bf
Aleksei Patsuk <aleksei.patsuk@ukg.com>
2023-06-16 01:01:53 +0300

    add 'list'

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,18 @@
+import * as fs from "node:fs";
+
 const list = async () => {
-    // Write your code here 
+  const dir = "./files";
+
+  fs.readdir(dir, (err, files) => {
+    if (err) {
+      console.error(new Error("FS operation failed"));
+      return;
+    }
+
+    files.forEach((file) => {
+      console.log(file);
+    });
+  });
 };
 
 await list();
