9d09336f557c4c9e957ea38923b11383a5f66e5c
Aleksei Patsuk <aleksei.patsuk@ukg.com>
2023-06-17 18:01:58 +0300

    add 'compress'

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,12 @@
+import fs from 'node:fs';
+import * as zlib from 'node:zlib';
+
 const compress = async () => {
-    // Write your code here 
+  const rs = fs.createReadStream('./files/fileToCompress.txt', 'utf-8');
+  const ws = fs.createWriteStream('./files/archive.gz');
+  const gzip = zlib.createGzip();
+  
+  rs.pipe(gzip).pipe(ws);
 };
 
 await compress();
