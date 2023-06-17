4c1334dc63843923e7fc59810da0f7a1bd9ae3ba
Aleksei Patsuk <aleksei.patsuk@ukg.com>
2023-06-17 17:44:56 +0300

    add 'transform'

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,8 +1,18 @@
+import { Transform, pipeline } from 'node:stream';
+
 const transform = async () => {
   process.stdin.setEncoding('utf-8');
-  process.stdin.on('data', (data) => {
-    process.stdout.write(data.split('').reverse().join(''));
+
+  const input = process.stdin;
+  const output = process.stdout;
+
+  const reverseTransform = new Transform({
+    transform(chunk, encoding, callback) {
+      callback(null, chunk.toString().trim().split('').reverse().join(''));
+    },
   });
+
+  pipeline(input, reverseTransform, output, (err) => console.log(err));
 };
 
 await transform();
