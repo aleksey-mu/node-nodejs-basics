399a5f33a418cc44b94d750da7416070b2ad0dd4
Aleksei Patsuk <aleksei.patsuk@ukg.com>
2023-06-18 14:27:14 +0300

    fix: slice length

--- src/cli/args.js
+++ src/cli/args.js
@@ -4,7 +4,7 @@ const parseArgs = () => {
 
   for (let i = 2; i < args.length; i = i + 2) {
     paramsArray.push(
-      `${args[i].slice(2, args[i].length - 1)} is ${args[i + 1]}`
+      `${args[i].slice(2, args[i].length)} is ${args[i + 1]}`
     );
   }
   console.log(paramsArray.join(', '));
